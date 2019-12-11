cbb5700ae5a6b740f9ffcbd30beaa007966efefd
ghnimi1 <ghnimih@gmail.com>
2019-12-11 10:11:40 +0100

    Updates

---
+++ precache-manifest.20fcdcf04d1981dea38f7c941132cc89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac83f5d3a6e3f94e29919b5049833112",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "93cf37e2b04d74791971",
+    "url": "/e-commerce/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1bb619fb6781c1933b83",
+    "url": "/e-commerce/static/css/main.c0fdbc1e.chunk.css"
+  },
+  {
+    "revision": "93cf37e2b04d74791971",
+    "url": "/e-commerce/static/js/2.102a9f83.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/e-commerce/static/js/2.102a9f83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bb619fb6781c1933b83",
+    "url": "/e-commerce/static/js/main.99985d5f.chunk.js"
+  },
+  {
+    "revision": "a4728a41d8aa748b2b5a",
+    "url": "/e-commerce/static/js/runtime-main.a27c3ceb.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/e-commerce/static/media/logo.9f7e506d.svg"
+  }
+]);
